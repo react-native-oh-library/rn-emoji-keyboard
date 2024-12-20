7bb4f7f3bfce8b4151d956ec267779e63db20b09
Pahkoo <851828489@qq.com>
2024-12-20 10:29:47 +0800

    chore: add README.OpenSource and OAT.XML and COMMITTERS.md and README.md (#12)

--- src/hooks/useKeyboard.ts
+++ src/hooks/useKeyboard.ts
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd. All rights reserved
+ * Use of this source code is governed by a MIT license that can be
+ * found in the LICENSE file.
+ */
 import { useCallback, useEffect, useState } from 'react'
 import { Keyboard, type KeyboardEvent,type KeyboardEventName,Platform } from 'react-native'
 
